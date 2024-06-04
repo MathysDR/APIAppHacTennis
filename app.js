e8f8e01a05dfd427359824b6cd4a1d36e720a092
Mathys <mathys.derrien1@gmail.com>
2024-06-04 15:38:08 +0200

    switch port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "http://192.168.1.26:3000"
 };
 
 app.use(cors(corsOptions));
@@ -23,7 +23,7 @@ app.get("/", (req, res) => {
 require("./app/routes/members.routes.js")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
